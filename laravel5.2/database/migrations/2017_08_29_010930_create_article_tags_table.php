daa58c0bf123d91cdb424521d04d5475bb35e520
lgj101 <1733948498@qq.com>
2017-08-29 11:06:45 +0800

    手记表迁移文件添加

---
+++ laravel5.2/database/migrations/2017_08_29_010930_create_article_tags_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_tags', function (Blueprint $table) {
+            $table->increments('id')->comment('标签ID');
+            $table->string('tag_name',20)->comment('标签名');
+            $table->integer('tag_num')->comment('标签所属手记数量');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('article_tags');
+    }
+}
