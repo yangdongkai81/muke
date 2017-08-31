28d692ecad2f7d59f8ebc0112a344dd3afad7376
root <yangdong2597@qq.com>
2017-08-31 14:45:00 +0800

    删

--- laravel5.2/public/js/list.js
+++
@@ -1,1 +0,0 @@
-define(function(require){require("common");$(document).on("mouseenter",".szcourse-cart",function(){var c=$(this);c.siblings(".active").removeClass("active"),c.addClass("active")}).on("mouseleave",".szcourse-cart",function(){})});
