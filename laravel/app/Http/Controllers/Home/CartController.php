bb06dbac4d7fdecfc1fd7ee85a13d0c3b869417b
liu <836656663@qq.com>
2017-09-13 14:35:22 +0800

    cart

--- laravel/app/Http/Controllers/Home/CartController.php
+++ laravel/app/Http/Controllers/Home/CartController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Home;
 
 use Illuminate\Http\Request;
 
+use DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -14,10 +16,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class CartController extends Controller
 {
     public function Index(){
-//        echo 1;die;
-        return view('Home/Cart/index');
+    	$cart = DB::table('shoppingcart')->get();
+        return view('Home/Cart/index',['cart'=>$cart]);
     }
-    function del(){
+
+    /*
+     * 购买
+     * */
+    public function Add($id){
 
     }
+    /*
+     * 购物车  删除
+     * */
+     function del(Request $request){
+        $id = $request->id;
+        $res = DB::table('shoppingcart')->where('sid',$id)->delete();
+
+         return $res ? 1 : 2;
+     }
 }
--- laravel/database/migrations/2017_08_30_062936_create_shoppingcart_table.php
+++ laravel/database/migrations/2017_08_30_062936_create_shoppingcart_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Shoppingcart extends Migration
+class CreateShoppingcartTable extends Migration
 {
     /**
      * Run the migrations.
@@ -17,7 +17,6 @@ class Shoppingcart extends Migration
             $table->increments('sid')->comment('购物车主键自增ID');
             $table->integer('pid')->comment('课程编号');
             $table->string('price',20)->comment('课程价格');
-            $table->string('totalprice',20)->comment('课程总价格');
             $table->integer('userid')->comment('用户编号');
         });
     }
