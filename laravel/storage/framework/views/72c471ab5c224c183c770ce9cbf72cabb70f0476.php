33b695e6c5aa5cebfec54bfbb84f9522d9186bfa
fsa <2131@qq.com>
2017-09-14 12:05:11 +0800

    del all

--- laravel/storage/framework/views/72c471ab5c224c183c770ce9cbf72cabb70f0476.php
+++
@@ -1,9 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<center>
-<h2 style="color:red">欢迎来到后台！！！</h2>
-</center>
-<?php $__env->stopSection(); ?>
-
-
-
-<?php echo $__env->make('adminlayout.index', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
